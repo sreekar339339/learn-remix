b89eb10088e0f017ef94847f765f74720ad3ca37
sreekar339339 <sreekar.nimbalkar@lacework.net>
2026-06-18 04:12:45 +0530

    cleanup

--- app/actions/todoListPage.tsx
+++ app/actions/todoListPage.tsx
@@ -1,12 +1,15 @@
-import { TodolistIndex } from "../assets/todolist/index.tsx";
+import { Frame } from "remix/ui";
+import { AddTodo } from "../assets/todolist/addTodo.tsx";
 import { Layout } from "../ui/layout.tsx";
+import { routes } from "../routes.ts";
 
 export function TodoListPage() {
   return () => (
     <Layout>
       <section>
         <h1>Todo-list</h1>
-        <TodolistIndex />
+        <AddTodo />
+        <Frame name="todoItems" src={routes.todolist.todos.index.href()} />
       </section>
     </Layout>
   )
---
+++ app/assets/todolist/addTodo.tsx
@@ -0,0 +1,59 @@
+import {
+  clientEntry,
+  css,
+  Frame,
+  on,
+  ref,
+  type Dispatched,
+  type Handle,
+} from "remix/ui";
+import { routes } from "../../routes.ts";
+
+export const AddTodo = clientEntry(
+  import.meta.url,
+  function AddTodo(handle: Handle) {
+    let input: HTMLInputElement;
+
+    let submit = async (
+      evt: Dispatched<SubmitEvent, HTMLFormElement>,
+      signal: AbortSignal,
+    ) => {
+      evt.preventDefault();
+      let form = evt.currentTarget;
+      const formAction = new URL(form.action);
+      formAction.searchParams.set("redirectTo", "none");
+      try {
+        let resp = await fetch(formAction, {
+          method: "POST",
+          body: new FormData(form),
+          signal,
+        });
+        if (!resp.ok) new Error(resp.statusText, { cause: resp.status });
+        if (signal.aborted) return;
+        await handle.frames.get("todoItems")?.reload();
+        form.reset();
+      } catch (error) {
+        console.error(error);
+      } finally {
+      }
+    };
+    return () => (
+      <form
+        method="POST"
+        action={routes.todolist.todos.action.href(undefined, {
+          intent: "create",
+        })}
+        mix={[on("submit", submit)]}
+      >
+        <label>
+          Enter a todo{" "}
+          <input
+            mix={[css({ padding: 4 }), ref((node) => (input = node))]}
+            name="text"
+            autofocus
+          />
+        </label>
+      </form>
+    );
+  },
+);
